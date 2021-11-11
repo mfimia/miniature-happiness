ecf3676b6ae4fcb93801fe97bfd13f8676046406
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-11 18:13:57 +0100

    added getters and setters

--- main.js
+++ main.js
@@ -9,6 +9,31 @@ class List {
   constructor(name, items) {
     this._name = name;
     this._items = items || [];
-    this._length = this._items._length;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  set items(item) {
+    this._items.push(item);
+  }
+
+  set name(name) {
+    this._name = name;
   }
 }
+
+const test = new List("test", ["1", 2, 3]);
+console.log(test.name);
+test.name = "dude";
+console.log(test.name);
+test.items = "hi";
+test.items = "hi";
+console.log(test.items);
+console.log(test.items.length);
+console.log(typeof test);
